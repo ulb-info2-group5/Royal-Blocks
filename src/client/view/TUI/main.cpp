d36f89c016523dbe67fd331be593a74cfe4a207c
ethan <ethanvr3@gmail.com>
2025-02-12 22:31:29 +0100

    add one todo

--- src/client/view/TUI/main.cpp
+++ src/client/view/TUI/main.cpp
@@ -6,6 +6,7 @@ int main() {
     std::shared_ptr<ftxui::ScreenInteractive> screen = std::shared_ptr<ftxui::ScreenInteractive>(
         new ftxui::ScreenInteractive(ftxui::ScreenInteractive::Fullscreen())
     );
+    // TODO: add loginInput to the loginMenu
     LoginMenu loginMenu = LoginMenu(screen);
     loginMenu.render();
     return 0;
