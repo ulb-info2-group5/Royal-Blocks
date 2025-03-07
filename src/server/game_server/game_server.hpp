f4bbf0e28467a8809e0b6cd01a151c59d9d3d5e9
lulu <lucas.verbeiren@gmail.com>
2025-03-07 11:59:44 +0100

    removed dead code

--- src/server/game_server/game_server.hpp
+++ src/server/game_server/game_server.hpp
@@ -15,19 +15,6 @@
 #include "player_state/player_state.hpp"
 
 #include <boost/asio.hpp>
-#include <memory>
-#include <variant>
-
-using PlayerID = size_t;
-
-using GameStatePtr = std::shared_ptr<GameState>;
-
-// All the events that the GameServer could receive.
-using EventVariant =
-    std::variant<bindings::BigDrop, bindings::BuyBonus, bindings::BuyPenalty,
-                 bindings::EmptyPenaltyStash, bindings::HoldNextTetromino,
-                 bindings::MoveActive, bindings::RotateActive,
-                 bindings::SelectTarget>;
 
 class GameServer {
   private:
