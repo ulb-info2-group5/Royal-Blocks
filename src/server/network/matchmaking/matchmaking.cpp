92b3bb5f83a6e9b7a2c8368f0686c872d63bd000
lulu <lucas.verbeiren@gmail.com>
2025-03-13 21:26:51 +0100

    finally updating screen, fix for server (unitialized player counter)
    unitialized player counter was in matchmaking

--- src/server/network/matchmaking/matchmaking.cpp
+++ src/server/network/matchmaking/matchmaking.cpp
@@ -1,117 +1,117 @@
 #include "matchmaking.hpp"
 // ====== Loby class ======
 
-
-
-
-
 // ====== GameCandidate class ======
 
-GameCandidate::GameCandidate(RequestJoinGame joinGame) 
-:  numberOfPlayersMax_{(joinGame.bindGame.gameMode == GameMode::Dual) ?  MAXPLAYERDUAL : MAXPLAYERCLASSICANDROYAL }, gameMode{joinGame.bindGame.gameMode} {
+GameCandidate::GameCandidate(RequestJoinGame joinGame)
+    : numberOfPlayerTotale_{0},
+      numberOfPlayersMax_{(joinGame.bindGame.gameMode == GameMode::Dual)
+                              ? MAXPLAYERDUAL
+                              : MAXPLAYERCLASSICANDROYAL},
+      gameMode{joinGame.bindGame.gameMode} {
     std::cout << "gameCandidate create (RequestJoinGame)" << std::endl;
-    numberOfPlayerTotale_ ++;
+    numberOfPlayerTotale_++;
     players_.push_back(joinGame.playerId);
 }
 
-GameCandidate::GameCandidate(RequestCreateGame createGame) : numberOfPlayersMax_{createGame.bindCreateGame.targetNumPlayers}, gameMode{createGame.bindCreateGame.gameMode} {
+GameCandidate::GameCandidate(RequestCreateGame createGame)
+    : numberOfPlayersMax_{createGame.bindCreateGame.targetNumPlayers},
+      gameMode{createGame.bindCreateGame.gameMode} {
     std::cout << "gameCandidate create (RequestCreateGame)" << std::endl;
-    numberOfPlayerTotale_ ++;
+    numberOfPlayerTotale_++;
     players_.push_back(createGame.playerId);
 }
 
-bool GameCandidate::tryToAddPlayer(RequestJoinGame joinGame ){
-    if (isThereRoomInThisGame()){
+bool GameCandidate::tryToAddPlayer(RequestJoinGame joinGame) {
+    if (isThereRoomInThisGame()) {
         players_.push_back(joinGame.playerId);
-        numberOfPlayerTotale_ ++;
+        numberOfPlayerTotale_++;
         std::cout << "add player " << std::endl;
         return true;
     }
     return false;
 }
 
-bool GameCandidate::isThereRoomInThisGame(){
+bool GameCandidate::isThereRoomInThisGame() {
     return numberOfPlayerTotale_ < numberOfPlayersMax_;
 }
 
-
-bool GameCandidate::isThisPartyReady(){
+bool GameCandidate::isThisPartyReady() {
     return numberOfPlayersMax_ == numberOfPlayerTotale_;
 }
 
-
-bool GameCandidate::isthisPlayerInThisGame(PlayerID playerId){
-    for (auto id : players_){
+bool GameCandidate::isthisPlayerInThisGame(PlayerID playerId) {
+    for (auto id : players_) {
         if (id == playerId) return true;
     }
     return false;
 }
 
+std::vector<PlayerID> &GameCandidate::getPlayers() { return players_; }
 
-std::vector<PlayerID>& GameCandidate::getPlayers(){
-    return players_;
-}
-
-GameMode GameCandidate::getGameMode(){
-    return gameMode;
-}
+GameMode GameCandidate::getGameMode() { return gameMode; }
 
 // ======= matchmaking class =======
 
-
-void Matchmaking::addPlayer(RequestJoinGame joinGame, GamesManager& gamesManager){
+void Matchmaking::addPlayer(RequestJoinGame joinGame,
+                            GamesManager &gamesManager) {
     findaGame(getGame(joinGame.bindGame.gameMode), joinGame, gamesManager);
 }
 
-std::vector<GameCandidate>& Matchmaking::getGame(GameMode gameMode){
-    switch (gameMode)
-    {
+std::vector<GameCandidate> &Matchmaking::getGame(GameMode gameMode) {
+    switch (gameMode) {
     case GameMode::Classic:
         return gamesCanditatesClassic_;
-        
+
     case GameMode::Dual:
         return gamesCanditatesDuel_;
-        
+
     case GameMode::RoyalCompetition:
         return gamesCanditatesRoyalCompetition_;
-        
+
     default:
         std::cerr << "incorect gamesMode" << std::endl;
         break;
     }
 }
 
-void Matchmaking::findaGame(std::vector<GameCandidate>& games, RequestJoinGame joinGame, GamesManager& gamesManager){
+void Matchmaking::findaGame(std::vector<GameCandidate> &games,
+                            RequestJoinGame joinGame,
+                            GamesManager &gamesManager) {
     bool joinFriend = joinGame.bindGame.friendId.has_value(), findGame = false;
     auto it = games.begin();
-    while (it != games.end() && !findGame){
-        if (joinFriend){
-            if (it->isthisPlayerInThisGame(joinGame.bindGame.friendId.value())){
+    while (it != games.end() && !findGame) {
+        if (joinFriend) {
+            if (it->isthisPlayerInThisGame(
+                    joinGame.bindGame.friendId.value())) {
                 findGame = it->tryToAddPlayer(joinGame);
             }
-        }else {
+        } else {
             findGame = it->tryToAddPlayer(joinGame);
         }
-        if (it->isThisPartyReady()){
+        if (it->isThisPartyReady()) {
             std::cout << "game ready" << std::endl;
             startGame(std::move(*it), gamesManager);
             it = games.erase(it);
-        }else {
+        } else {
             ++it;
         }
     }
     if (!findGame) createNewGameCandidate(games, joinGame);
 }
 
-void Matchmaking::createNewGameCandidate(std::vector<GameCandidate>& games, RequestJoinGame joinGame){
+void Matchmaking::createNewGameCandidate(std::vector<GameCandidate> &games,
+                                         RequestJoinGame joinGame) {
     games.push_back(GameCandidate{joinGame});
 }
 
-
-void Matchmaking::createAGame(RequestCreateGame createGame){
-    getGame(createGame.bindCreateGame.gameMode).push_back(GameCandidate{createGame});
+void Matchmaking::createAGame(RequestCreateGame createGame) {
+    getGame(createGame.bindCreateGame.gameMode)
+        .push_back(GameCandidate{createGame});
 }
 
-void Matchmaking::startGame(GameCandidate &&gameCandidate, GamesManager& gamesManager){
-    gamesManager.startGameServeur(gameCandidate.getGameMode(), gameCandidate.getPlayers());
+void Matchmaking::startGame(GameCandidate &&gameCandidate,
+                            GamesManager &gamesManager) {
+    gamesManager.startGameServeur(gameCandidate.getGameMode(),
+                                  gameCandidate.getPlayers());
 }
