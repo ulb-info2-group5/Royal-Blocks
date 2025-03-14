2ab82024cd2594fbd827c1eed51b08cf9943c15b
lulu <lucas.verbeiren@gmail.com>
2025-03-14 13:06:54 +0100

    added missing nlohmann include in cmake, removed glaze, using preinstalled sqlite3

--- third_party/glaze.cmake
+++
@@ -1,9 +0,0 @@
-include(FetchContent)
-
-FetchContent_Declare(glaze
-  GIT_REPOSITORY https://github.com/stephenberry/glaze.git
-  GIT_TAG main
-  GIT_SHALLOW TRUE
-)
-
-FetchContent_MakeAvailable(glaze)
