61c4f70b7b7d5672e171630a1a9d1bdb3c30d582
lulu <lucas.verbeiren@gmail.com>
2025-03-19 15:46:30 +0100

    pinning ftxui commit

--- third_party/ftxui.cmake
+++ third_party/ftxui.cmake
@@ -2,8 +2,8 @@ include(FetchContent)
 
 FetchContent_Declare(ftxui
   GIT_REPOSITORY https://github.com/ArthurSonzogni/ftxui
-  GIT_TAG main
-  GIT_SHALLOW TRUE
+  GIT_TAG 8519e9b0f31ac8f5765a3fc63c3b80a5cfbe9d6d
+  GIT_SHALLOW FALSE
 )
 
 FetchContent_MakeAvailable(ftxui)
