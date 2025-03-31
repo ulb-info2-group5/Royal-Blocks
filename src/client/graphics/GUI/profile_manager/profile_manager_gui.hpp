431033f8d15cee3065bcb9fa5463a6cbf15a3df1
Ernest Malysz <ernest.malysz@ulb.be>
2025-03-31 20:32:45 +0200

    v1 header pour profile_manager Ernest Jonas Rafaou

---
+++ src/client/graphics/GUI/profile_manager/profile_manager_gui.hpp
@@ -0,0 +1,41 @@
+#ifndef PROFILE_MANAGER_GUI_HPP
+#define PROFILE_MANAGER_GUI_HPP
+
+#include <QApplication>
+#include <QPushButton>
+#include <QWidget>
+#include <QLineEdit>
+#include <QLabel>
+
+class Controller;
+
+class MainGui;
+
+class ProfileManagerGui : public QWidget {
+    Q_OBJECT
+
+    private:
+        Controller &controller_;
+        MainGui &mainGui_;
+
+        QStackedWidget *stack_;
+        QWidget *mainWidget_;
+
+        QPushButton submit_;
+        QPushButton back_;
+
+        QLineEdit newUserName_;
+        QLineEdit newPassWord_;
+
+        QLabel changePasswordAndUsernameMsgLabel_;
+
+
+    public:
+        ProfileManagerGui(Controller &controller, MainGui &mainGui, QWidget *parent = nullptr);
+
+        ~ProfileManagerGui() = default;
+
+    signals:
+        // Back to main menu
+        void backToMainMenu();
+}
