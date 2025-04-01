968d3b8ea1e6638ddb224e0dba22025b1e4416fe
ethan <ethanvr3@gmail.com>
2025-04-01 16:52:39 +0200

    add try catch with throw error in main.cpp

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -3,6 +3,14 @@
 #include <QApplication>
 #include <iostream>
 
+constexpr char noArgument[] = "\033[1;31merror:\033[0m no flag specified\nTry './tetris_royal_client --help' for more information.";
+constexpr char invalidNumberOfArguments[] = "\033[1;31merror:\033[0m invalid number of arguments\nTry './tetris_royal_client --help' for more information.";
+constexpr char invalidFlag []= "\033[1;31merror:\033[0m invalid flag specified\nTry './tetris_royal_client --help' for more information.";
+constexpr char CHAR_TUI[] = "--tui";
+constexpr char CHAR_GUI[] = "--gui";
+constexpr char CHAR_HELP[] = "--help";
+constexpr char helpInfo[] = "Tetris Royal : Help\n\nUsage: tetris_royal_client [OPTION]\n\nOptions:\n  --help\t\tDisplay this information\n  --gui\t\t\tStart the game with a graphical user interface\n  --tui\t\t\tStart the game with a terminal user interface";
+
 /*
  * @brief Handle the arguments passed to the program
  *
@@ -12,60 +20,40 @@
  */
 UiChoice handleArguments(int argc, char *argv[]) {
     if (argc == 1) {
-        std::cerr << "Tetris Royal : No arguments provided. See '" << argv[0]
-                  << " --help' for more information." << std::endl;
+        throw std::invalid_argument(noArgument);
     }
 
     else if (argc != 2) {
-
-        std::cerr << "Tetris Royal : Invalid number of arguments. Expected one "
-                     "argument. See '"
-                  << argv[0] << " --help' for more information." << std::endl;
+        throw std::invalid_argument(invalidNumberOfArguments);
     }
 
-    else if (std::string(argv[1]) == "--help") {
-        std::cout << "Tetris Royal : Help" << std::endl;
-
-        std::cout << std::endl;
-
-        std::cout << "Usage: tetris_royal_client [OPTION]" << std::endl;
-
-        std::cout << std::endl;
-
-        std::cout << "Options:" << std::endl;
-        std::cout << "  --help\t\tDisplay this information" << std::endl;
-        std::cout
-            << "  --gui\t\t\tStart the game with a graphical user interface"
-            << std::endl;
-        std::cout
-            << "  --tui\t\t\tStart the game with a terminal user interface"
-            << std::endl;
-    }
-
-    else if (std::string(argv[1]) == "--gui") {
+    else if (strcmp(argv[1], CHAR_GUI) == 0) {
         return UiChoice::GUI;
     }
 
-    else if (std::string(argv[1]) == "--tui") {
+    else if (strcmp(argv[1], CHAR_TUI) == 0) {
         return UiChoice::TUI;
     }
 
-    else {
-        std::cerr << "Tetris Royal : '" << argv[1]
-                  << "' is not a valid argument. See '" << argv[0]
-                  << " --help' for more information." << std::endl;
+    else if (strcmp(argv[1], CHAR_HELP) == 0) {
+        std::cout << helpInfo << std::endl;
+        return UiChoice::NONE;
     }
 
-    return UiChoice::NONE;
+    throw std::invalid_argument(invalidFlag);
 }
 
 int main(int argc, char *argv[]) {
-    UiChoice choice = handleArguments(argc, argv);
-
-    if (choice == UiChoice::NONE) {
-        return -1;
+    try {    
+        UiChoice choice = handleArguments(argc, argv);
+        if (choice == UiChoice::NONE) {
+            return EXIT_SUCCESS;
+        }
+        Controller controller(choice, std::make_pair(argc, argv));
+        return controller.run();
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
     }
-
-    Controller controller(choice, std::make_pair(argc, argv));
-    return controller.run();
+    return EXIT_SUCCESS;
 }
