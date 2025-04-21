96447457cc4f9983a1371785aa9d4c01f515f19e
lulu <lucas.verbeiren@gmail.com>
2025-04-21 16:11:59 +0200

    update_type doc

--- src/client/core/controller/update_type.hpp
+++ src/client/core/controller/update_type.hpp
@@ -1,6 +1,11 @@
 #ifndef UPDATE_TYPE_HPP
 #define UPDATE_TYPE_HPP
 
+/**
+ * @brief The differents things that can be updated.
+ *
+ * This is used by the Controller and display-components to talk to each other.
+ */
 enum class UpdateType {
     FRIENDS_LIST,
     FRIEND_REQUESTS,
