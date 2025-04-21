3670acfb098fe19daf1856faba308b407759de69
lulu <lucas.verbeiren@gmail.com>
2025-04-21 16:00:10 +0200

    minor change gui.cpp

--- src/client/gui.cpp
+++ src/client/gui.cpp
@@ -6,12 +6,12 @@
 
 int main(int argc, char *argv[]) {
     try {
-
         Controller controller(std::make_unique<GUI::MainGui>(argc, argv));
         controller.run();
-
     } catch (const std::exception &e) {
         std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }
+
+    return EXIT_SUCCESS;
 }
