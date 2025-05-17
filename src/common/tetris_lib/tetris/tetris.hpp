274aaade70f468f83b26a3299428cad1990bde27
evr5 <ethanvr3@gmail.com>
2025-05-17 13:04:32 +0200

    add GPL header to all .cpp and .hpp

--- src/common/tetris_lib/tetris/tetris.hpp
+++ src/common/tetris_lib/tetris/tetris.hpp
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Royal Blocks.
+ *
+ * Royal Blocks is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Royal Blocks is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Royal Blocks.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef TETRIS_HPP
 #define TETRIS_HPP
 
